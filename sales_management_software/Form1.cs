29f73bf7f413a33270a98fddd631a3d10fe25a1d
nntrung47 <nntrung47@gmail.com>
2022-05-23 11:40:45 +0700

    Change label1

--- sales_management_software/Form1.cs
+++ sales_management_software/Form1.cs
@@ -16,5 +16,10 @@ namespace sales_management_software
         {
             InitializeComponent();
         }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            label1.Text = "Demo push";
+        }
     }
 }
